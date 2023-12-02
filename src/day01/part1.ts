68b62d5b3aea7987148759521077f14a85d9aab9
wpdough <me@wpdough.com>
2023-12-03 02:48:02 +0900

    solve day 1 part 1

---
+++ src/day01/part1.ts
@@ -0,0 +1,23 @@
+import { Solver } from "../types";
+
+export default class Part1 implements Solver {
+  solve(input: string[]): number {
+    const calibrationValues = input
+      .map(this.extractDigits)
+      .map(this.determineCalibrationValue);
+    return calibrationValues.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  private extractDigits(line: string): number[] {
+    return line
+      .split("")
+      .map((char) => parseInt(char))
+      .filter((num) => num);
+  }
+
+  private determineCalibrationValue(digits: number[]): number {
+    const firstAndLastDigit = [digits[0], digits[digits.length - 1]];
+    const calibrationValue = parseInt(firstAndLastDigit.join(""));
+    return calibrationValue;
+  }
+}
